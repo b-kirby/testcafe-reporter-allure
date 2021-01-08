767c237c6fe6881441fb51e527768ebfefa82a7d
Ben Kirby <benkirby86@gmail.com>
2021-01-08 12:29:03 -0500

    Added dist folder

---
+++ dist/testcafe/models.d.ts
@@ -0,0 +1,44 @@
+export {};
+declare global {
+    interface TestController {
+        testRun: TestRun;
+    }
+    interface TestRun {
+        test: Test;
+    }
+    interface Test {
+        meta: object;
+    }
+}
+export interface Screenshot {
+    screenshotPath?: string;
+    thumbnailPath?: string;
+    userAgent?: string;
+    quarantineAttempt?: number;
+    takenOnFail?: boolean;
+}
+export interface Video {
+    singleFile?: boolean;
+    testRunId?: string;
+    videoPath?: string;
+}
+export interface TestRunInfo {
+    videos: Video[];
+    errs?: object[];
+    warnings?: string[];
+    durationMs?: number;
+    unstable?: boolean;
+    screenshotPath?: string;
+    screenshots?: Screenshot[];
+    quarantine?: object;
+    skipped?: boolean;
+}
+export interface ErrorObject {
+    errMsg?: string;
+    callsite?: CallSite;
+    userAgent?: string;
+}
+export interface CallSite {
+    filename?: string;
+    lineNum?: string;
+}
